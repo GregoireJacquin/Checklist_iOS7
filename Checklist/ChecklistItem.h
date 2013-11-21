3157504c866e2cc4e3679fc49d8c6b72505b939b
Mac Dev <macpao2@iMac-de-macdev.local>
2013-11-21 14:59:00 +0100

    Array

---
+++ Checklist/ChecklistItem.h
@@ -0,0 +1,13 @@
+//
+//  ChecklistItem.h
+//  Checklist
+//
+//  Created by Mac Dev on 21/11/2013.
+//  Copyright (c) 2013 RetroTouch. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ChecklistItem : NSObject
+
+@end
---
+++ Checklist/ChecklistItem.m
@@ -0,0 +1,13 @@
+//
+//  ChecklistItem.m
+//  Checklist
+//
+//  Created by Mac Dev on 21/11/2013.
+//  Copyright (c) 2013 RetroTouch. All rights reserved.
+//
+
+#import "ChecklistItem.h"
+
+@implementation ChecklistItem
+
+@end
