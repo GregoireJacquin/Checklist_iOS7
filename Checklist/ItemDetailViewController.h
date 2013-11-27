78065bd084e52b17a073b31c2243eb92b34e9352
Mac Dev <macpao2@iMac-de-macdev.local>
2013-11-27 15:34:40 +0100

    Editing item

---
+++ Checklist/ItemDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddItemViewController.h
+//  Checklist
+//
+//  Created by Mac Dev on 27/11/2013.
+//  Copyright (c) 2013 RetroTouch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddItemViewController : UITableViewController
+
+@end
